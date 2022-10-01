f90da64053ff02009c234c0061349320fb63dcb3
Ilus <doomlordcs@gmail.com>
2022-10-01 14:06:14 +0100

    Move accept header to config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 const config = {
     userAgent:
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.91 Safari/537.36',
+    acceptHeader:
+        'video/mp4, video/webm;q=0.9, image/jpeg, image/png, image/gif;q=0.8, application/octet-stream;q=0.7, */*;q=0.6, text/html;q=0.5',
 }
 
 export default config
