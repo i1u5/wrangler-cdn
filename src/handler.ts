d314759e8d8f9bade545fb064cb0eb690ec4b79a
Ilus <doomlordcs@gmail.com>
2022-10-01 14:09:17 +0100

    Pass URL origin

--- src/handler.ts
+++ src/handler.ts
@@ -3,13 +3,13 @@ import { jsonError } from './jsonError'
 
 export async function handleRequest(event: FetchEvent): Promise<Response> {
     const { request } = event
-    const url = new URL(request.url)
-    const path = url.pathname.substring(1)
+    const uri = new URL(request.url)
+    const path = uri.pathname.substring(1)
 
     try {
         if (!path) return jsonError('No Filename', 406)
 
-        return await image(path, event)
+        return await image(uri, event)
     } catch (err: any) {
         return jsonError(err.toString(), 500)
     }
